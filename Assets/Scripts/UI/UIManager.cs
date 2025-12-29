36a693995ce939361f89ca3de3175e38b4dcf7b4
Zaituny <mohamedinv.580@gmail.com>
2025-12-29 21:46:06 +0200

    fixed bug with rotation sliders where values weren't being used correctly

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -4,28 +4,25 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] Transform engine;
 
+    private Vector3 rot = Vector3.zero;
+
     public void OnScaleSliderValueChanged(float value) {
         // scale engine to value
         engine.localScale = Vector3.one * value;
     }
 
     public void OnRotateXSliderValueChanged(float value) {
-        Vector3 rot = engine.eulerAngles;
         rot.x = value;
-        Debug.Log(value);
-        Debug.Log(rot);
         engine.eulerAngles = rot;
     }
 
     public void OnRotateYSliderValueChanged(float value)
     {
-        Vector3 rot = engine.eulerAngles;
         rot.y = value;
         engine.eulerAngles= rot;
     }
     public void OnRotateZSliderValueChanged(float value)
     {
-        Vector3 rot = engine.eulerAngles;
         rot.z = value;
         engine.eulerAngles = rot;
     }
